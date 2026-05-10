a8c378f93f40fd2def6d82cae1a1afc983ad46f9
S-lentOne <mellomember@gmail.com>
2026-05-10 12:26:51 +0800

    Update

--- shiru/sources/sukebeisrc/index.js
+++ shiru/sources/sukebeisrc/index.js
@@ -57,7 +57,7 @@ export default new (class SukebeiSrc extends AbstractSource {
       seeders: Number(item.Seeders || 0),
       leechers: Number(item.Leechers || 0),
       downloads: Number(item.Downloads || 0),
-      size: 0,
+      size: parseSize(item.Size),
       date: new Date(item.DateUploaded),
       accuracy: "medium",
       type: "alt",
